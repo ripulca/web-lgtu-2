6a05018930c12d8dd0ed40772f3030c6eedd2b6b
ripulca <reneitpa@gmail.com>
2021-10-18 15:41:33 +0300

    ...

--- js/script.js
+++ js/script.js
@@ -1,15 +1,30 @@
 $(function () {
+    $('.header_nav_auth_btn').click(function () {
+        if(!$('.dark_background').hasClass('open')){ 
+            $('.dark_background').addClass(' open');
+            $('.enter').addClass(' open');
+        }
+    })
+
     $('.sign_in').click(function () {
-        $('.dark_background').addClass(' open');
-        $('.enter').addClass(' open');
+        if(!$('.enter').hasClass('open')){ 
+            $('.enter').addClass(' open');
+        }        
+        if($('.registration').hasClass('open')){ 
+            $('.registration').removeClass(' open');
+        }
     })
 
     $('.sign_up').click(function () {
-        $('.dark_background').addClass(' open');
-        $('.registration').addClass(' open');
+        if(!$('.registration').hasClass('open')){ 
+            $('.registration').addClass(' open');
+        }        
+        if($('.enter').hasClass('open')){ 
+            $('.enter').removeClass(' open');
+        }
     })
 
-    $('.modal_win_close_btn').click(function () {
+    $('.close').click(function () {
         $('.dark_background').removeClass(' open');
         if($('.enter').hasClass('open')){ 
             $('.enter').removeClass(' open');
@@ -33,51 +48,45 @@ $('.dark_background').mouseup(function (e) {
     }
   });
 
-function login_validation(login){
-    var loginformat = /^[A-Za-z]+$/;
-    if(login.value.match(loginformat)){
-        return true;
-    }
-    else{
-        alert('Login must have alphabet characters only');
-        login.focus();
-        return false;
-    }
-}
-
-function pwd_validation(pwd, min, max){
+function pwd_validation(pwd){
+    num_amount=0;
     var pwd_len = pwd.value.length;
-    if (pwd_len == 0 ||pwd_len>= min || pwd_len<max){
-        alert("Password should not be empty / length be between "+min+" to "+max);
-        pwd_len.focus();
-        return false;
+    for(var i=0; i<pwd_len; i++){
+        if(isNaN(pwd.value[i])){
+            num_amount++;
+        }
     }
-    return true;
-}
-
-function email_validation(email){
-    var mailformat = /^((([0-9A-Za-z]{1}[-0-9A-z\.]{1,}[0-9A-Za-z]{1})|([0-9А-Яа-я]{1}[-0-9А-я\.]{1,}[0-9А-Яа-я]{1}))@([-0-9A-Za-z]{1,}\.){1,2}[-A-Za-z]{2,})$/;
-    if(email.value.match(mailformat)){
+    if(pwd_len>num_amount){
         return true;
     }
-    else{
-        alert("You have entered an invalid email address!");
-        email.focus();
-        return false;
-    }
+    alert('Login must have ');
+    pwd.focus();
+    return false;
 }
 
-function phone_validation(phone){
-    var phoneformat= /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/;
-    if(phone.value.match(phoneformat)){
-        return true;
-    }
-    else{
-        alert('Phone is not correct!');
-        phone.focus();
-        return false;
-    }
-}
+// function email_validation(email){
+//     var mailformat = /^((([0-9A-Za-z]{1}[-0-9A-z\.]{1,}[0-9A-Za-z]{1})|([0-9А-Яа-я]{1}[-0-9А-я\.]{1,}[0-9А-Яа-я]{1}))@([-0-9A-Za-z]{1,}\.){1,2}[-A-Za-z]{2,})$/;
+//     if(email.value.match(mailformat)){
+//         return true;
+//     }
+//     else{
+//         alert("You have entered an invalid email address!");
+//         email.focus();
+//         return false;
+//     }
+// }
+
+// function phone_validation(phone){
+//     var phoneformat= /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/;
+//     if(phone.value.match(phoneformat)){
+//         return true;
+//     }
+//     else{
+//         alert('Phone is not correct!');
+//         phone.focus();
+//         return false;
+//     }
+// }
 
 function password_repeat_validation(pwd, pwd_repeat) {
     if(pwd.value==pwd_repeat.value){
@@ -90,30 +99,22 @@ function password_repeat_validation(pwd, pwd_repeat) {
 
 function formValidation(){
     if($('.registration').hasClass('open')){
-        var login = document.registration.login;
+        // var login = document.registration.login;
         var pwd = document.registration.password;
         var pwd_repeat=document.registration.password_repeat;
-        var email = document.registration.email;
-        var phone = document.registration.phone;
-        if(login_validation(login)){
-            if(email_validation(email)){
-                if(phone_validation(phone)){
-                    if(pwd_validation(pwd)){
-                        if(password_repeat_validation(pwd, pwd_repeat)){
-                            console.log('ok');
-                        }
-                    }
-                }
+        // var email = document.registration.email;
+        // var phone = document.registration.phone;
+        if(pwd_validation(pwd)){
+            if(password_repeat_validation(pwd, pwd_repeat)){
+                console.log('ok');                    
             }
         }
     }
     else{
-        var login = document.enter.login;
+        // var login = document.enter.login;
         var pwd = document.enter.password;
-        if(login_validation(login)){
-            if(pwd_validation(pwd)){
-                console.log('ok');
-            }
+        if(pwd_validation(pwd)){
+            console.log('ok');
         }
     }
 }
